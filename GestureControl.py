45eaf2e43d43ac3e71448fa2cd194d3f278ba012
Cheran-Rammohan <39864586+Cheran-Rammohan@users.noreply.github.com>
2022-03-21 22:23:21 -0400

    Initial commit

--- GestureControl.py
+++ GestureControl.py
@@ -1,6 +1,6 @@
 import cv2
-import HandTrackingModule2 as htm
 import os
+from cvzone.HandTrackingModule import HandDetector
 
 #variables
 width, height = 640, 480
@@ -13,11 +13,25 @@ cap.set(4, height)
 
 #   Accessing the fingers
 folderPath = "FingerImages"
-fingerImages = sorted(os.listdir(folderPath))                       #Accesses all the fingerImages
+fingerImages = sorted(os.listdir(folderPath), key=len)                       #Accesses all the fingerImages
 print(fingerImages)
+
+#   Variables
+imgNumber = 0
+
+#   Hand Detector
+detector = HandDetector(detectionCon=0.8, maxHands=1)
 
 while True:
     success, img = cap.read()
+
+    hands, img = detector.findHands(img)            #calls findHands method
+
+    if hands:                                       #if there are hands
+        hand = hands[0]                             #only one hand
+        fingers = detector.fingersUp(hand)          #calls fingersUp method to see how many fingers are up
+        print(fingers)                              #prints list of fingers that are up
+
     cv2.imshow("Image", img)
     key = cv2.waitKey(1)
     if key == ord('q'):
